f587d0aeeb0c9bfd6ab53a3cfffebf626d47aa02
Felipe Moreira da Cruz <piradous@gmail.com>
2023-10-17 16:46:32 -0300

    feat: resolucao projeto

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia nokia = new Nokia("9920-2228", "Nokia", "123456", 8);
+Iphone iphone = new Iphone("9917-2491", "Iphone 13", "654321", 16);
+
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Whatsapp");
+
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Whatsapp");
